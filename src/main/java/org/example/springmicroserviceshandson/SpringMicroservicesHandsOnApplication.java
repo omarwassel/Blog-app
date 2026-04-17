105934c9c29891bdddf3a4e8502df7f9f73635ea
omar-wasel <omar.wassel@apexmedical.ai>
2026-04-18 00:11:26 +0200

    Initial commit

---
+++ src/main/java/org/example/springmicroserviceshandson/SpringMicroservicesHandsOnApplication.java
@@ -0,0 +1,13 @@
+package org.example.springmicroserviceshandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesHandsOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springmicroserviceshandson/SpringMicroservicesHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springmicroserviceshandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
