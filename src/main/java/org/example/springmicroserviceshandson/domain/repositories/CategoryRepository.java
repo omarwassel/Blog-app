c4ac10a8eb65ea91d94f4f8abdcba630c2fcd708
omar-wasel <omar.wassel@apexmedical.ai>
2026-04-19 03:44:19 +0200

    add repos interfaces

---
+++ src/main/java/org/example/springmicroserviceshandson/domain/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package org.example.springmicroserviceshandson.domain.repositories;
+
+import org.example.springmicroserviceshandson.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+}
---
+++ src/main/java/org/example/springmicroserviceshandson/domain/repositories/PostRepository.java
@@ -0,0 +1,12 @@
+package org.example.springmicroserviceshandson.domain.repositories;
+
+import org.example.springmicroserviceshandson.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+
+}
